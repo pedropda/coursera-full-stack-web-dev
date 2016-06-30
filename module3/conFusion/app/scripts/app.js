004159433bd77a111d35607794044d571feb75a7
Lori <lori.karikari@student.pxl.be>
2016-06-30 20:35:27 +0200

    angular

--- module3/conFusion/app/scripts/app.js
+++ module3/conFusion/app/scripts/app.js
@@ -1,73 +1,71 @@
 'use strict';
-angular.module('confusionApp', [])
 
-        .controller('menuController',['$scope', function($scope) {
-            this.tab = 1;
-            this.filtText = '';
-            $scope.showDetails = false;
+angular.module('confusionApp', ['ui.router','ngResource'])
+.config(function($stateProvider, $urlRouterProvider) {
+        $stateProvider
 
-            var dishes=[
-                         {
-                          name:'Uthapizza',
-                          image: 'images/uthapizza.png',
-                          category: 'mains',
-                          label:'Hot',
-                          price:'4.99',
-                          description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
-                           comment: ''
-                        },
-                        {
-                           name:'Zucchipakoda',
-                           image: 'images/zucchipakoda.png',
-                           category: 'appetizer',
-                           label:'',
-                           price:'1.99',
-                          description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce',
-                           comment: ''
-                        },
-                        {
-                           name:'Vadonut',
-                           image: 'images/vadonut.png',
-                           category: 'appetizer',
-                           label:'New',
-                           price:'1.99',
-                          description:'A quintessential ConFusion experience, is it a vada or is it a donut?',
-                           comment: ''
-                        },
-                        {
-                           name:'ElaiCheese Cake',
-                           image: 'images/elaicheesecake.png',
-                          category: 'dessert',
-                           label:'',
-                          price:'2.99',
-                          description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
-                           comment: ''
-                        }];
-
-            $scope.dishes = dishes;
-
-            $scope.toggleDetails = function() {
-                           $scope.showDetails = !$scope.showDetails;
-                       };
-
-             this.select = function(setTab) {
-                this.tab = setTab;
-                if (setTab === 2) {
-                    this.filtText = "appetizer";
+            // route for the home page
+            .state('app', {
+                url:'/',
+                views: {
+                    'header': {
+                        templateUrl : 'views/header.html',
+                    },
+                    'content': {
+                        templateUrl : 'views/home.html',
+                        controller  : 'IndexController'
+                    },
+                    'footer': {
+                        templateUrl : 'views/footer.html',
+                    }
                 }
-                else if (setTab === 3) {
-                    this.filtText = "mains";
-                }
-                else if (setTab === 4) {
-                    this.filtText = "dessert";
-                }
-                else {
-                    this.filtText = "";
-                }
-            };
-            this.isSelected = function (checkTab) {
-                return (this.tab === checkTab);
-            };
 
+            })
 
-        }]);
+            // route for the aboutus page
+            .state('app.aboutus', {
+                url:'aboutus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/aboutus.html',
+                        controller  : 'AboutController'
+                    }
+                }
+            })
+
+            // route for the contactus page
+            .state('app.contactus', {
+                url:'contactus',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/contactus.html',
+                        controller  : 'ContactController'
+                    }
+                }
+            })
+
+            // route for the menu page
+            .state('app.menu', {
+                url: 'menu',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/menu.html',
+                        controller  : 'MenuController'
+                    }
+                }
+            })
+
+            // route for the dishdetail page
+            .state('app.dishdetails', {
+                url: 'menu/:id',
+                views: {
+                    'content@': {
+                        templateUrl : 'views/dishdetail.html',
+                        controller  : 'DishDetailController'
+                   }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/');
+    })
+;
--- module3/conFusion/gulpFile.js
+++ module3/conFusion/gulpFile.js
@@ -77,7 +77,7 @@ gulp.task('browser-sync', ['default'], function () {
    browserSync.init(files, {
       server: {
          baseDir: "dist",
-         index: "menu.html"
+         index: "index.html"
       }
    });
         // Watch any files in dist/, reload on change
@@ -85,7 +85,7 @@ gulp.task('browser-sync', ['default'], function () {
     });
 
 gulp.task('usemin',['jshint'], function () {
-  return gulp.src('./app/menu.html')
+  return gulp.src('./app/**/*.html')
       .pipe(usemin({
         css:[minifycss(),rev()],
         js: [ngannotate(),uglify(),rev()]
